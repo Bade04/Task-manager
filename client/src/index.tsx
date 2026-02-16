8b30788065e8e38d8af57c8c4ea1df399a6da254
Bade04 <adekoyaajibade@gmail.com>
2026-02-17 00:41:46 +0100

    error fixed now

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,13 +4,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Analytics } from "@vercel/analytics/react";
-
-// Add inside return
-<>
-  <App />
-  <Analytics />
-</>;
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
